cc0490d300eeb0d20d68a0002b257ea360ad4957
JenjaRad <a0040@ukr.net>
2022-02-10 00:34:43 +0200

    Add additional fields to FlightRequest.java

--- src/main/java/com/eugene/flight/domain/request/FlightRequest.java
+++ src/main/java/com/eugene/flight/domain/request/FlightRequest.java
@@ -1,9 +1,11 @@
 package com.eugene.flight.domain.request;
 
 import com.eugene.flight.domain.FlightStatus;
+import lombok.Builder;
 
 import java.time.Instant;
 
+@Builder
 public record FlightRequest(FlightStatus status, String companyName, Instant createdAt, Instant delayAt,
-                            Instant endedAt) {
+                            Instant endedAt, String departureCountry, int distance, Long id) {
 }
