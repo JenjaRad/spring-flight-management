d9fb87dba08c58bc8b6ff61b152faa135506afa2
JenjaRad <a0040@ukr.net>
2022-05-17 00:22:48 +0300

    add static import to web test

--- src/test/java/com/eugene/flight/util/TestUtil.java
+++ src/test/java/com/eugene/flight/util/TestUtil.java
@@ -8,35 +8,31 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public final class TestUtil {
 
-    private static final byte MIN_ORIGIN = 1;
-    private static final byte MAX_BOUND = 11;
+    private static final short MIN_ORIGIN = 1;
+    private static final short MAX_BOUND = 11;
 
     private TestUtil() {
     }
 
     public static AirCompany airCompanyBuilder(Long id, String name) {
-        return AirCompany
-                .builder()
-                .id(id)
-                .name(name)
-                .build();
+        if (id < MIN_ORIGIN) {
+            throw new IllegalArgumentException("You cannot set negative id to the company");
+        }
+        return AirCompany.builder().id(id).name(name).build();
     }
 
     public static AirCompanyRequest airCompanyRequestBuilder(Long id, String name) {
-        return AirCompanyRequest
-                .builder()
-                .id(id)
-                .name(name)
-                .build();
+        if (id < MIN_ORIGIN) {
+            throw new IllegalArgumentException("You cannot set negative id to the companyRequest");
+        }
+        return AirCompanyRequest.builder().id(id).name(name).build();
     }
 
     public static Long generateRandomNumber() {
-        return ThreadLocalRandom.current()
-                .nextLong(MIN_ORIGIN, MAX_BOUND);
+        return ThreadLocalRandom.current().nextLong(MIN_ORIGIN, MAX_BOUND);
     }
 
     public static String generateRandomString() {
-        return UUID.randomUUID()
-                .toString();
+        return UUID.randomUUID().toString();
     }
 }
